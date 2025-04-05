2cd98efb80911e233b9670a4a2bc5115e87c3eb7
NotRealGuest <139834084+AaronVerdep@users.noreply.github.com>
2025-04-05 17:31:34 -0400

    Create loadinganim.py

---
+++ pkgs/loadinganim.py
@@ -0,0 +1,22 @@
+#loadinganim.py
+def loadinganim(times, clear):
+    for i in range(times):
+            if clear == True:
+              os.system('clear')
+            print('-')
+            time.sleep(0.1)
+            if clear == True:
+              os.system('clear')
+            print('/')
+            time.sleep(0.1)
+            if clear == True:
+              os.system('clear')
+            print('|')
+            time.sleep(0.1)
+            if clear == True:
+              os.system('clear')
+            print('\ ')
+            time.sleep(0.1)
+            if clear == True:
+              os.system('clear')
+            print('-')
