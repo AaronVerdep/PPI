2668ef73cbaa5a8a7a005565624135c6fe660ed3
NotRealGuest <139834084+AaronVerdep@users.noreply.github.com>
2025-04-06 12:49:07 -0400

    Update menu.py

--- pkgs/menu.py
+++ pkgs/menu.py
@@ -4,15 +4,17 @@ import os
 menu_data = {
     'options': []  # each option has 'label' and 'function'
 }
+title = "MENU"
 
 def add_option(label, function):
     menu_data['options'].append({'label': label, 'function': function})
 
 def display_menu():
     os.system('clear')
-    print("MENU:")
+    print(f"{title}:")
     for i, option in enumerate(menu_data['options']):
         print(f"{i + 1}. {option['label']}")
+    print()
     choice = input("> ")
     if choice.isdigit():
         index = int(choice) - 1
@@ -22,3 +24,17 @@ def display_menu():
     os.system('clear')
     print("Invalid option.")
     input('')
+
+def set_title(settitle):
+    title = settitle
+
+def clear_menu():
+    menu_data = {
+        'options': []
+    }
+
+def reset_menu():
+    title = "MENU"
+    menu_data = {
+        'options': []
+    }
