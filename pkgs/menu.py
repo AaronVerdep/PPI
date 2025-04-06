a820f651fc5cb09b1e5351d224643826f0a76cb6
NotRealGuest <139834084+AaronVerdep@users.noreply.github.com>
2025-04-06 12:58:28 -0400

    Update menu.py

--- pkgs/menu.py
+++ pkgs/menu.py
@@ -10,7 +10,7 @@ def add_option(label, function):
     menu_data['options'].append({'label': label, 'function': function})
 
 def display_menu():
-    os.system('clear')
+    os.system('clear' if os.name != 'nt' else 'cls')
     print(f"{title}:")
     for i, option in enumerate(menu_data['options']):
         print(f"{i + 1}. {option['label']}")
@@ -21,19 +21,22 @@ def display_menu():
         if 0 <= index < len(menu_data['options']):
             menu_data['options'][index]['function']()
             return
-    os.system('clear')
+    os.system('clear' if os.name != 'nt' else 'cls')
     print("Invalid option.")
     input('')
 
 def set_title(settitle):
+    global title
     title = settitle
 
 def clear_menu():
+    global menu_data
     menu_data = {
         'options': []
     }
 
 def reset_menu():
+    global title, menu_data
     title = "MENU"
     menu_data = {
         'options': []
